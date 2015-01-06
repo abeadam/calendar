6048fbd6fa1a1cf26b373bdc8f53311e5cbb321f
abraham <abeadam89@gmail.com>
2015-01-06 03:55:01 -0800

    finished styling

---
+++ dist/.module-cache/e223fa85767371944ea5726d67cbf8f9e0f7754c.js
@@ -0,0 +1,24 @@
+var Calendar = Calendar || {};
+// this will represent a single list item 
+Calendar.EventItem = Backbone.Model.extend({
+    initialize: function() {
+        var self = this,
+            timeSpan = self.get('end') - self.get('start');
+        self.updateWidth();
+        self.set('pixelHeight', timeSpan * Calendar.EventItem.hourPixelMapping);
+        self.set('y', self.get('start') * Calendar.EventItem.hourPixelMapping);
+    },
+    updateWidth: function() {
+        var self = this;
+        self.set('pixelWidth', Calendar.EventItem.maxWidth / self.get('sharedItems'));
+        self.set('x', self.get('itemNumber') * self.get('pixelWidth'));
+    },
+    updateShared: function(shared) {
+        var self = this;
+        self.set('sharedItems', shared);
+        self.updateWidth();
+    }
+}, {
+    maxWidth: 600,
+    hourPixelMapping: (30 * 2) / 60 // this is the pixels for 1 minute
+});
